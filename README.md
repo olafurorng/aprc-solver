54e36ef904e4fec0fc5c7e2aa6ff4600e79e9d61
Ólafur Örn <olafur@plainvanillagames.com>
2018-03-22 10:10:13 +0100

    Initializing project

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# aprc-solver
-APRC solver
+# APRC Solver
+*Licence: GNU GENERAL PUBLIC LICENSE (see `LICENCE` document) - No liability of usage*
+*Author: Olafur Orn Gudmundsson*
+
+APRC solver solves APRC out of the APRC formula (i.e. the X variable)
+
+![APRC formula](aprc_full_formula.png?raw=true "APRC formula")
+
+by using binary search. This solver assumes that
+all the loan is paid out in the beginning of the
+loan time so the formula can be simplified into:
+
+![APRC simplified formula](aprc_full_formula.png?raw=true "APRC simplified formula")
+
+
+## Usage
+### Example of usage
+```
+import { calcAprc } from 'aprc-solver'
+
+const monthlyPayments = [110, 107, 105, 102];
+const s = 380;
+
+const aprc = calcAprc(monthlyPayments, s)
+```
+
+### Details
+We use trial and error with binary search
+to find APRC, similar to the Goal Seek
+function in Excel. See formula for APRC:
+https://www.handbook.fca.org.uk/handbook/MCOB/10A/2.html?date=2016-06-30
+The formula assumes that there is monthly
+payments and the first payment is 1 month
+after loan is taken. It also assumes that
+every month in the year has equal length.
+
+ATTRIBUTES:
+@param payments: List of all the repayments
+                 back to loan provider.
+@param s: The s variable in the APRC formula.
+          The money which the person gets
+          in his pocket when taking the loan
+
+@return value between 0 and 1 (the APRC)
+        rounded to the fourth decimal place
+
+
+
+## Inspiration
---
+++ index.js
@@ -0,0 +1,60 @@
+const MAX_ITERATIONS = 50;
+
+/* Calculates APRC.
+ * We use trial and error with binary search
+ * to find APRC, similar to the Goal Seek
+ * function in Excel. See formula for APRC:
+ * https://www.handbook.fca.org.uk/handbook/MCOB/10A/2.html?date=2016-06-30
+ * The formula assumes that there is monthly
+ * payments and the first payment is 1 month
+ * after loan is taken. It also assumes that
+ * every month in the year has equal length.
+ *
+ * ATTRIBUTES:
+ * @param payments: List of all the repayments
+ *                  back to loan provider.
+ * @param s: The s variable in the aprc formula.
+ *           The money which the person gets
+ *           in his pocket when taking the loan
+ *
+ * @return value between 0 and 1 (the APRC)
+ *         rounded to the fourth decimal place
+ */
+export function calcAprc(payments, s) {
+  return calcAprcWithBinarySearch(payments, s, 0.0, 1.0, 0)
+}
+
+/*
+ * minValue = minimum possible aprc
+ * maxValue = maximum possible aprc
+*/
+function calcAprcWithBinarySearch(payments, s, minValue, maxValue, iterationLvl) {
+  // check if the interval is zero or less than certain decimal places
+  if ((maxValue - minValue) < 0.0001) { // rounded to the fourth decimal place
+    return minValue;
+  }
+
+  // guessing APRC with binary search
+  const aprc = (maxValue - minValue) / 2 + minValue;
+
+  // check if we have maximized the iterations
+  if (iterationLvl > MAX_ITERATIONS) {
+    return aprc;
+  }
+
+  const leftSideOfTheEquation = s;
+  let rightSideOfTheEquation = 0.0;
+  for (var i = 0; i < payments.length; i++) {
+    const Ak = payments[i];
+    rightSideOfTheEquation += Ak * Math.pow(1+ aprc, -(i+1.0) / 12.0);;
+  }
+
+  // check if the aprc is correct guessed
+  if (leftSideOfTheEquation == rightSideOfTheEquation) {
+    return aprc;
+  } else if (leftSideOfTheEquation < rightSideOfTheEquation) {
+    return calcAprcWithBinarySearch(payments, s, aprc, maxValue, ++iterationLvl);
+  } else { // leftSideOfTheEquation > rightSideOfTheEquation
+    return calcAprcWithBinarySearch(payments, s, minValue, aprc, ++iterationLvl);
+  }
+}
